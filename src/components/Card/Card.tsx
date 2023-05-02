54206041543843660ed840a8a10cfb795f0c14d1
Jonathan Hsu <jonathan.hsu@bell.ca>
2023-05-01 23:49:42 -0400

    card type badge & card title name

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,9 +9,14 @@ const Card: React.FC<CardProps> = ({ pokemon }) => {
   const imgURL = getImageURL(pokemon.id)
 
   return (
-    <div className="card">
+    <div className="card glass">
       <div className="card-title">
-        <h2>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
+        <h2 className="card-title-name">{pokemon.name.replace(/-/g, ' ')}</h2>
+        <div>
+          {pokemon.types.map(({ type }) => (
+            <span className="type-badge">{type.name}</span>
+          ))}
+        </div>
       </div>
       <div className="card-image">
         <img alt={pokemon.name} src={imgURL} />
